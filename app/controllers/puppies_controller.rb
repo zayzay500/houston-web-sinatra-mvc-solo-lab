96c35043afd3904164b717d61d4ca6072d5b544b
zayzay500 <lindsay.pierce@yale.edu>
2020-06-16 16:26:42 -0400

    completed lab deliverables

--- app/controllers/puppies_controller.rb
+++ app/controllers/puppies_controller.rb
@@ -1,3 +1,51 @@
 class PuppiesController < ApplicationController
-   
+    get '/puppies' do
+        erb :display_puppies
+    end
+
+    get '/puppies/new' do
+        erb :new_puppy
+    end
+
+    get '/puppies/:id' do
+        # binding.pry
+        @puppy = find_puppy(params)
+
+        erb :show
+    end
+
+    get '/puppies/:id/edit' do
+        @puppy = find_puppy(params)
+
+        erb :edit_puppy
+    end
+
+    post '/puppies/' do
+        binding.pry
+        puppy = Puppy.create(params)
+
+        redirect "/puppies/#{puppy.id}"
+    end
+
+    patch '/puppies/:id' do
+        # binding.pry
+        puppy = Puppy.find(params[:id])
+        puppy.update(name: params[:name], breed: params[:breed])
+
+        redirect "/puppies/#{puppy.id}"
+    end
+
+    delete '/puppies/:id' do
+        # binding.pry
+        puppy = Puppy.find(params[:id])
+        puppy.delete
+
+        redirect '/puppies'
+    end
+
+    def find_puppy(params)
+        Puppy.find_by(params)
+    end
+
+
 end
--- config.ru
+++ config.ru
@@ -7,3 +7,4 @@ if ActiveRecord::Base.connection.migration_context.needs_migration?
 
 
 run ApplicationController
+run PuppiesController
